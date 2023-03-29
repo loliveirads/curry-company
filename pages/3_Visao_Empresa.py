c9c30eb374d80e0d391e29e5cca4f26326c21017
loliveirads <119435421+loliveirads@users.noreply.github.com>
2023-03-29 13:38:39 -0300

    Update 3_Visao_Empresa.py

--- pages/3_Visao_Empresa.py
+++ pages/3_Visao_Empresa.py
@@ -183,7 +183,7 @@ def density_map(df1):
 df1 = clean_data(df)
 
 
-with open('assets/css.css') as f:
+with open('css.css') as f:
   st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 
 
