6cfbeea1a2e9850da6a8be828dc3eb1e1e000a12
loliveirads <119435421+loliveirads@users.noreply.github.com>
2023-03-29 13:47:51 -0300

    Update 2_Visão_Entregadores.py

--- pages/2_Visão_Entregadores.py
+++ pages/2_Visão_Entregadores.py
@@ -179,7 +179,7 @@ def fig12(df1):
 # ========================
 df1 = clean_data(df)
 
-with open('css2.css') as f:
+with open('assets/css2.css') as f:
   st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 
 
